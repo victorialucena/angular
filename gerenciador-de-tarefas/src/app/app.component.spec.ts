3df4160e5a83783a0e7ed7f5d67140184ff8c459
victorialucena <lucenavictoria0@gmail.com>
2023-06-20 18:45:54 -0300

    add new projecct task manager

---
+++ gerenciador-de-tarefas/src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent],
+    schemas: [
+      CUSTOM_ELEMENTS_SCHEMA
+    ]
+  }));
+  TestBed.compileComponents();
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
---
+++ gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.spec.ts
@@ -0,0 +1,18 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditarTarefaComponent } from './editar-tarefa.component';
+
+describe('EditarTarefaComponent', () => {
+  let component: EditarTarefaComponent;
+  let fixture: ComponentFixture<EditarTarefaComponent>;
+
+  beforeEach(() => {
+    // TestBed.configureTestingModule({
+    //   declarations: [EditarTarefaComponent]
+    // });
+    // fixture = TestBed.createComponent(EditarTarefaComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+
+});
