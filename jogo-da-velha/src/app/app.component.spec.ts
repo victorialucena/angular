206bf1792121990f53237073f11ae6bf2e9a1cef
victorialucena <lucenavictoria0@gmail.com>
2023-07-04 14:31:39 -0300

    add new project hash

---
+++ jogo-da-velha/src/app/app.component.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent],
+    imports:[JogoDaVelhaModule]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
